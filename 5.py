f96966bdc68b1adf4edee205317265ff40ae345e
Eleveer <113224919+Eleveer@users.noreply.github.com>
2022-11-12 12:58:55 +0700

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,16 @@
+n = int(input('Введите n: '))
+for x in range(1,n+1):
+    if x == 1:
+        print('1'*n, end ='\n')
+    if x>1:
+        print('1','0'*(n-2),'1',sep='', end = '\n')
+    if x == n:
+        print('1'*n)
+a = int(input('Какой столбец вас интересует? - '))
+if a == 1:
+    print(n)
+if a > 1:
+    print('2')
+if a == n:
+    print(n)
+        
